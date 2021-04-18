7cf86624bfbf0f6fb72b7fad23b60aaffd8da3a8
Paulo Jabardo <pjabardo@gmail.com>
2021-04-18 09:06:20 -0300

    Got arduino to read 32 channels!
    Still need to develop an interface. I think I will use the Scanivalve as a template.

--- multianemdaq/multianemdaq.ino
+++ multianemdaq/multianemdaq.ino
@@ -8,46 +8,51 @@
 #include <SPI.h>
 #include <Mcp320x.h>
 
-#define SPI_CS1    	5 		   // SPI slave select
 #define SPI_MOSI1   23       
 #define SPI_MISO1   19
 #define SPI_CLK1    18
 
-#define SPI_CS2     26        // SPI slave select
+#define SPI_CS1      5        // SPI slave select
+#define SPI_CS2      22
+#define SPI_CS3      25
+#define SPI_CS4      26
+
 //#define SPI_MOSI2   13       
 //#define SPI_MISO2   12
 //#define SPI_CLK2    14
 
-#define ADC_VREF    3375     // 3.3V Vref
+#define ADC_VREF    2500     // 3.3V Vref
 #define ADC_CLK     1600000  // SPI clock 1.6MHz
 
+#define NCHANS 8
+#define NMCP 4
+#define TOTAL_CHANS 32
 
 const MCP3208::Channel chan_id[] = {MCP3208::Channel::SINGLE_0, MCP3208::Channel::SINGLE_1, MCP3208::Channel::SINGLE_2, MCP3208::Channel::SINGLE_3,
                            MCP3208::Channel::SINGLE_4, MCP3208::Channel::SINGLE_5, MCP3208::Channel::SINGLE_6, MCP3208::Channel::SINGLE_7};
                            
 
-                           
+
 SPIClass spi1(VSPI);
-//SPIClass spi2(VSPI);
 
 MCP3208 adc1(ADC_VREF, SPI_CS1, &spi1);
 MCP3208 adc2(ADC_VREF, SPI_CS2, &spi1);
+MCP3208 adc3(ADC_VREF, SPI_CS3, &spi1);
+MCP3208 adc4(ADC_VREF, SPI_CS4, &spi1);
 
-const int NCHANS = 8;
+int raw[TOTAL_CHANS];
 
 void setup() {
-
   // configure PIN mode
   pinMode(SPI_CS1, OUTPUT);
-
-  // set initial PIN state
-  digitalWrite(SPI_CS1, HIGH);
-
-  // configure PIN mode
   pinMode(SPI_CS2, OUTPUT);
+  pinMode(SPI_CS3, OUTPUT);
+  pinMode(SPI_CS4, OUTPUT);
 
-  // set initial PIN state
+  digitalWrite(SPI_CS1, HIGH);
   digitalWrite(SPI_CS2, HIGH);
+  digitalWrite(SPI_CS3, HIGH);
+  digitalWrite(SPI_CS4, HIGH);
 
 
   // initialize serial
@@ -57,15 +62,12 @@ void setup() {
   // initialize SPI interface for MCP3208
   SPISettings settings(ADC_CLK, MSBFIRST, SPI_MODE0);
   spi1.begin(SPI_CLK1, SPI_MISO1, SPI_MOSI1, SPI_CS1);
-  //spi2.begin(SPI_CLK1, SPI_MISO1, SPI_MOSI1, SPI_CS2);
   
   spi1.beginTransaction(settings);
-  //spi2.beginTransaction(settings);
 
 
 }
 
-int raw[16];
 
 
 const int NSAMPLES=1;
@@ -75,25 +77,34 @@ void loop() {
   uint32_t t2;
   // start sampling
   Serial.println("Reading...");
-  for (int i = 0; i < (2*NCHANS); ++i)
+  for (int i = 0; i < TOTAL_CHANS; ++i)
     raw[i] = 0;
   
   t1 = micros();
-  for (int i = 0; i < NSAMPLES; ++i){
+  int *r = raw;
+  for (int k = 0; k < NSAMPLES; ++k){
     for (int i = 0; i < 8; ++i)
     {
-      raw[i] += adc1.read(chan_id[i]);
+      *r++ += adc1.read(chan_id[i]);
     }
     for (int i = 0; i < 8; ++i)
     {
-      raw[i+8] += adc2.read(chan_id[i]);
+      *r++ += adc2.read(chan_id[i]);
+    }
+    for (int i = 0; i < 8; ++i)
+    {
+      *r++ += adc3.read(chan_id[i]);
+    }
+    for (int i = 0; i < 8; ++i)
+    {
+      *r++ += adc4.read(chan_id[i]);
     }
   }
   t2 = micros();
 
   // get analog value
 
-  for (int i = 0; i < (2*NCHANS); ++i)
+  for (int i = 0; i < TOTAL_CHANS; ++i)
   {
     Serial.print(i);
     Serial.print(") ");
